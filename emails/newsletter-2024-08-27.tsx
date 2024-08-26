d7e30a9a1de357e1cb804e166e80ca784b8397ca
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-08-26 17:04:29 +0100

    new template

---
+++ emails/newsletter-2024-08-27.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { render } from "@react-email/render";
+import { Html } from "@react-email/html";
+import { Head } from "@react-email/head";
+import { Body } from "@react-email/body";
+import { Container } from "@react-email/container";
+import { Img } from "@react-email/img";
+import { Text } from "@react-email/text";
+import { Hr } from "@react-email/hr";
+import { Link } from "@react-email/link";
+import { Heading } from "@react-email/heading";
+import { Button } from "@react-email/button";
+import style from "../style.js";
+
+interface Email_2024_07_24Props {
+  unsubscribeUrl: string;
+}
+
+export const Email_2024_07_24 = ({ unsubscribeUrl }: Email_2024_07_24Props) => (
+  <Html>
+    <Head />
+    <Body style={style.body}>
+      <Container style={style.container}>
+        <Img
+          src="https://nn1.dev/logo-email.png"
+          width="119"
+          height="28"
+          alt="NN1 Dev Club Logo"
+          style={style.img}
+        />
+        <Text style={style.text}>
+          Hey folks. We hope you had a lovely bank holiday weekend. Today is a
+          quick one to remind you about two things.
+        </Text>
+        <Hr style={style.hr} />
+        <Heading style={style.heading}>NN1 Dev Club co-working days</Heading>
+        <Text style={style.text}>
+          Thanks to the courtesy of Vulcan Works, we have an excellent
+          opportunity to use their co-working space for FREE every last Friday
+          of the month. High probability we will go for lunch together or a beer
+          afterwards.
+          <br />
+          <br />
+          The next one is this Friday, 30/08/2024. Just pop into the Vulcan
+          Works and register at the reception. Lovely girls will point you in
+          the right direction.
+        </Text>
+        <Hr style={style.hr} />
+        <Heading style={style.heading}>NN1 Dev Club #3 meet-up</Heading>
+        <Text style={style.text}>
+          The third meet-up is coming on Thursday, 26/09/2024. Other than superb
+          speakers, we have a bunch of extras prepared for you. Check our
+          website for more details and book your FREE ticket now.{" "}
+        </Text>
+        <Button style={style.button} href="https://nn1.dev/events/3">
+          Get your FREE ticket now
+        </Button>
+        <Text style={style.text}>
+          If you have questions, ping us on social media or reply to this email.
+          <br />
+          <br />
+          NN1 Dev Club Crew,
+          <br />
+          Pawel & Darren
+        </Text>
+        <Hr style={style.hr} />
+        <Text style={style.text}>
+          Are you no longer interested? You can{" "}
+          <Link style={style.link} href={unsubscribeUrl}>
+            unsubscribe
+          </Link>{" "}
+          anytime.
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+Email_2024_07_24.PreviewProps = {
+  unsubscribeUrl: "#",
+} as Email_2024_07_24Props;
+
+const renderEmail_2024_07_24 = (props: Email_2024_07_24Props) => ({
+  html: render(<Email_2024_07_24 {...props} />),
+  text: render(<Email_2024_07_24 {...props} />, { plainText: true }),
+});
+
+export default Email_2024_07_24;
+export { renderEmail_2024_07_24 };
