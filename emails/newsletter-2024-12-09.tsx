4dc35d8352d2a629f6911e32bea536b60b4758e9
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-12-09 10:08:32 +0000

    change date for the newsletter email

--- emails/newsletter-2024-12-09.tsx
+++ emails/newsletter-2024-12-09.tsx
@@ -52,10 +52,10 @@ Email.PreviewProps = {
   unsubscribeUrl: "#",
 } as EmailProps;
 
-const renderEmailNewsletter_2024_12_10 = async (props: EmailProps) => ({
+const renderEmailNewsletter_2024_12_09 = async (props: EmailProps) => ({
   html: await render(<Email {...props} />),
   text: await render(<Email {...props} />, { plainText: true }),
 });
 
 export default Email;
-export { renderEmailNewsletter_2024_12_10 };
+export { renderEmailNewsletter_2024_12_09 };
