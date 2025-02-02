6ac17f51c8a7ad1f74c8469806c7e90e12005ab0
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-02 21:00:17 +0000

    add new template 2025.02.03

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -8,16 +8,6 @@ function Socials() {
   return (
     <Row style={{ display: "table-cell", paddingBottom: "26px" }}>
       <Column style={{ paddingRight: "13px" }}>
-        <Link href="https://github.com/nn1-dev">
-          <Img
-            src="https://nn1.dev/emails/github.png"
-            width="39"
-            height="39"
-            alt="NN1 Dev Club Logo"
-          />
-        </Link>
-      </Column>
-      <Column style={{ paddingRight: "13px" }}>
         <Link href="http://linkedin.com/company/nn1-dev">
           <Img
             src="https://nn1.dev/emails/linkedin.png"
@@ -51,6 +41,16 @@ function Socials() {
         <Link href="https://chat.nn1.dev/">
           <Img
             src="https://nn1.dev/emails/discord.png"
+            width="39"
+            height="39"
+            alt="NN1 Dev Club Logo"
+          />
+        </Link>
+      </Column>
+      <Column style={{ paddingRight: "13px" }}>
+        <Link href="https://github.com/nn1-dev">
+          <Img
+            src="https://nn1.dev/emails/github.png"
             width="39"
             height="39"
             alt="NN1 Dev Club Logo"
