8147417879ad02616b67f1ce993c99816036be98
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-07 09:13:09 +0100

    simplify feedback email template

--- emails/admin-feedback.tsx
+++ emails/admin-feedback.tsx
@@ -5,11 +5,8 @@ import Text from "../components/Text.tsx";
 import Hr from "../components/Hr.tsx";
 
 interface EmailAdminFeedbackProps {
-  name?: string;
-  stack?: string;
-  who: string;
-  interval: string;
-  feedback?: string;
+  name: string;
+  feedback: string;
 }
 
 export const EmailAdminFeedback = ({
@@ -22,7 +19,7 @@ export const EmailAdminFeedback = ({
     <Text>
       <strong>Name:</strong>
       <br />
-      {name ? name : "Anonymous"}
+      {name}
     </Text>
     <Text>
       <strong>Feedback:</strong>
@@ -34,9 +31,6 @@ export const EmailAdminFeedback = ({
 
 EmailAdminFeedback.PreviewProps = {
   name: "Pablo Picasso",
-  stack: "AWS, Rust, JavaScript",
-  who: "Pro",
-  interval: "Every 3 months is fine",
   feedback:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
 } as EmailAdminFeedbackProps;
